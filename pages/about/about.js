4e81a70a7371e6ce1a79f0513e2a4fa385fa6462
钟仕垣 <958460248@qq.com>
2017-11-19 20:30:06 +0800

    小程序功能完整实现

--- pages/about/about.js
+++ pages/about/about.js
@@ -62,13 +62,6 @@ Page({
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-  
   detail: function (e) {
     var data = e.currentTarget.dataset;
     var url = data.url;
--- pages/joke/joke.js
+++ pages/joke/joke.js
@@ -70,13 +70,6 @@ Page({
   },
 
   /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
-  /**
    * 请求数据
    */
   requestData: function () {
